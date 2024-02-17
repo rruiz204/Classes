4635689c2bad274f08184ad70fefd25bd8dba68e
roney ruiz <roney.ruiz@golabstech.com>
2024-02-17 00:28:47 -0600

    python - variables

---
+++ python/variables.py
@@ -0,0 +1,54 @@
+"""
+Topic: Data Types and Variables
+- Integer
+- String
+- Float
+- Boolean
+"""
+
+# EXAMPLES
+integer_var = 100
+
+string_var = "Hello World!"
+
+float_var = 100.50
+
+boolean_var = True
+
+
+"""
+Topics: Constanst
+"""
+
+MESSAGE = "Hello"
+
+def function(person):
+	print(f"${MESSAGE} ${person}!")
+
+function("Brittany")
+function("Goku")
+
+"""
+If you dont know what does this code, 
+you can do some research about functions and F-Strings
+"""
+
+
+"""
+Topic: Type Casting
+You can investigate what is Type Casting in Python
+"""
+
+num_a = 5.4
+num_b = 4.3
+
+result = num_a + num_b
+print(int(result))
+
+"""
+Investigate about these functions
+- int()
+- str()
+- float()
+- bool()
+"""
