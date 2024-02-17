dec426dc8ab262752e397447927452577fab622f
roney ruiz <roney.ruiz@golabstech.com>
2024-02-17 11:09:38 -0600

    python - arrays

---
+++ python/arrays.py
@@ -0,0 +1,31 @@
+"""
+Topic: Arrays
+The arrays are multiple values in a single variable
+The arrays can contain many types of data
+"""
+
+array = [1, 2, 3, "Fuck you", True, False, "Hi :3"]
+
+# We can access any item in the array with the Index
+print(array[-1], array[3])
+
+# The ararys have many methods, for example...
+
+# Add an item
+array_a = [1, 2, 3]
+print(array_a)
+
+array_a.append(4)
+print(array_a)
+
+# Remove an item
+array_b = [7, 6, 5]
+print(array_b)
+
+array_b.pop()
+print(array_b)
+
+"""
+You can to see all methods here:
+https://www.w3schools.com/python/python_ref_list.asp
+"""
